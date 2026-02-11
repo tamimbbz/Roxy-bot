4ca209f3b0cb7a9fd8b80e03eefb4f7da81cdc6b
tamimbbz <tanishajannat721615@gmail.com>
2026-02-12 01:24:33 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100081088184521", "61583129938292",];
+		const permission = ["100087466441450", "61563627004291",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" khankir chele ja vaggg 🐤", event.threadID, event.messageID);
 		}
